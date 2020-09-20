13592cac797b4edf547e7b3bf6d80373d6e2e882
nbena <ciaobena@outlook.it>
2020-09-20 13:08:49 +0200

    repository_test.go: finish porting

--- repository_test.go
+++ repository_test.go
@@ -2,6 +2,7 @@ package smallben
 
 import (
 	"errors"
+	"fmt"
 	"github.com/stretchr/testify/suite"
 	"gorm.io/gorm"
 	"testing"
@@ -94,6 +95,41 @@ func (r *RepositoryOtherTestSuite) TestResume() {
 	r.Equal(len(r.availableUserEvaluationRules), len(rules), "Len mismatch")
 }
 
+func (r *RepositoryOtherTestSuite) TestSetCronId() {
+	counter := 10
+	testsBefore := make([]Test, 0)
+	for _, rule := range r.availableUserEvaluationRules {
+		for i := range rule.Tests {
+			rule.Tests[i].CronId = counter
+			counter += 1
+			testsBefore = append(testsBefore, rule.Tests[i])
+		}
+	}
+
+	err := r.repository.SetCronIdOf(r.availableUserEvaluationRules)
+	r.Nil(err, "Cannot set cron id of")
+
+	rules, err := r.repository.GetAllUserEvaluationRulesToExecute()
+	r.Nil(err, "Cannot retrieve rules")
+
+	testsAfter := flatTests(rules)
+	flags := make([]bool, len(testsAfter))
+
+	for i, testBefore := range testsBefore {
+		for _, testAfter := range testsAfter {
+			if testBefore.Id == testAfter.Id {
+				r.Equal(testBefore.CronId, testAfter.CronId, "CronId failed")
+				flags[i] = true
+				break
+			}
+		}
+	}
+	for i, flag := range flags {
+		r.Equal(flag, true, fmt.Sprintf("Not flagged: %v", testsBefore[i]))
+	}
+
+}
+
 func TestRepositoryTestSuite(t *testing.T) {
 	suite.Run(t, new(RepositoryAddTestSuite))
 }
@@ -134,210 +170,3 @@ func setup(suite suite.Suite) (Repository, []UserEvaluationRule) {
 	}
 	return repository, availableUserEvaluationRules
 }
-
-//import (
-//	"fmt"
-//	. "github.com/onsi/ginkgo"
-//	. "github.com/onsi/gomega"
-//	"testing"
-//)
-//
-//func TestRepository(t *testing.T) {
-//	RegisterFailHandler(Fail)
-//	RunSpecs(t, "Repository")
-//}
-//
-//var _ = Describe("Repository", func() {
-//	var repository Repository
-//	var availableUserEvaluationRules []UserEvaluationRule
-//
-//	BeforeEach(func() {
-//		var err error
-//
-//		repositoryOptions := NewRepositoryOptions()
-//		repository, err = NewRepository(&repositoryOptions)
-//		Expect(err).ShouldNot(HaveOccurred())
-//
-//		availableUserEvaluationRules = []UserEvaluationRule{
-//			{
-//				Id:     1,
-//				UserId: 1,
-//				Tests: []Test{
-//					{
-//						Id:                   2,
-//						EverySecond:          60,
-//						UserId:               1,
-//						UserEvaluationRuleId: 1,
-//						Paused:               false,
-//					}, {
-//						Id:                   3,
-//						EverySecond:          120,
-//						UserId:               1,
-//						UserEvaluationRuleId: 1,
-//						Paused:               false,
-//					},
-//				},
-//			},
-//		}
-//	})
-//
-//	// now test the adding of UserEvaluationRule
-//	Context("when adding user evaluation rules", func() {
-//
-//		It("adds correctly those evaluation rules", func() {
-//
-//			// add them...
-//			err := repository.AddUserEvaluationRule(availableUserEvaluationRules)
-//			Expect(err).ShouldNot(HaveOccurred())
-//
-//			// now performs a select making sure the adding is ok
-//			result, err := repository.GetAllUserEvaluationRulesToExecute()
-//			Expect(err).ShouldNot(HaveOccurred())
-//			Expect(len(result)).To(Equal(len(availableUserEvaluationRules)))
-//
-//		})
-//
-//		Context("retrieves it", func() {
-//			BeforeEach(func() {
-//				err := repository.AddUserEvaluationRule(availableUserEvaluationRules)
-//				Expect(err).ShouldNot(HaveOccurred())
-//			})
-//
-//			It("without errors", func() {
-//				_, err := repository.GetUserEvaluationRule(availableUserEvaluationRules[0].Id)
-//				Expect(err).ShouldNot(HaveOccurred())
-//			})
-//		})
-//
-//		AfterEach(func() {
-//			// and we delete them to be sure we leave room for
-//			// other operations
-//			err := repository.DeleteUserEvaluationRules(getIdsFromUserEvaluationRuleList(availableUserEvaluationRules))
-//			Expect(err).ShouldNot(HaveOccurred())
-//		})
-//
-//	})
-//
-//	Context("when deleting user evaluation rules", func() {
-//
-//		BeforeEach(func() {
-//			err := repository.AddUserEvaluationRule(availableUserEvaluationRules)
-//			Expect(err).ShouldNot(HaveOccurred())
-//		})
-//
-//		It("deletes  correctly those evaluation rules", func() {
-//
-//			// perform a delete
-//			err := repository.DeleteUserEvaluationRules(getIdsFromUserEvaluationRuleList(availableUserEvaluationRules))
-//			Expect(err).ShouldNot(HaveOccurred())
-//
-//			// and then make sure those rules are not in the result
-//			all, err := repository.GetAllUserEvaluationRulesToExecute()
-//			Expect(err).ShouldNot(HaveOccurred())
-//			allIds := getIdsFromUserEvaluationRuleList(all)
-//			Expect(allIds).ToNot(ContainElements(getIdsFromUserEvaluationRuleList(availableUserEvaluationRules)))
-//		})
-//	})
-//
-//	Context("pausing user evaluation rules", func() {
-//
-//		BeforeEach(func() {
-//
-//			err := repository.AddUserEvaluationRule(availableUserEvaluationRules)
-//			Expect(err).ShouldNot(HaveOccurred())
-//
-//		})
-//
-//		It("correctly pauses those user evaluation rules", func() {
-//
-//			// we set them to paused
-//			// without need to modify the models
-//			err := repository.PauseUserEvaluationRules(availableUserEvaluationRules)
-//			Expect(err).ShouldNot(HaveOccurred())
-//
-//			// now we retrieve them
-//			rules, err := repository.GetAllUserEvaluationRulesToExecute()
-//			Expect(err).ShouldNot(HaveOccurred())
-//
-//			Expect(getIdsFromUserEvaluationRuleList(rules)).ToNot(ContainElements(getIdsFromUserEvaluationRuleList(availableUserEvaluationRules)))
-//		})
-//
-//		It("correctly resume those user evaluation rules", func() {
-//
-//			// re-pause them
-//			err := repository.PauseUserEvaluationRules(availableUserEvaluationRules)
-//			Expect(err).ShouldNot(HaveOccurred())
-//
-//			err = repository.ResumeUserEvaluationRule(availableUserEvaluationRules)
-//			Expect(err).ShouldNot(HaveOccurred())
-//
-//			// now we retrieve them
-//			rules, err := repository.GetAllUserEvaluationRulesToExecute()
-//			Expect(err).ShouldNot(HaveOccurred())
-//
-//			Expect(len(availableUserEvaluationRules)).To(Equal(len(rules)))
-//		})
-//
-//		AfterEach(func() {
-//			// and we delete them to be sure we leave room for
-//			// other operations
-//			err := repository.DeleteUserEvaluationRules(getIdsFromUserEvaluationRuleList(availableUserEvaluationRules))
-//			Expect(err).ShouldNot(HaveOccurred())
-//		})
-//	})
-//
-//	Context("when setting the cron id of tests", func() {
-//
-//		var testsBefore []Test
-//
-//		BeforeEach(func() {
-//			// add the UserEvaluationRule
-//			err := repository.AddUserEvaluationRule(availableUserEvaluationRules)
-//			Expect(err).ShouldNot(HaveOccurred())
-//
-//			counter := 10
-//			for _, rule := range availableUserEvaluationRules {
-//				for i := range rule.Tests {
-//					rule.Tests[i].CronId = counter
-//					counter += 1
-//					testsBefore = append(testsBefore, rule.Tests[i])
-//				}
-//			}
-//		})
-//
-//		It("works", func() {
-//
-//			err := repository.SetCronIdOf(availableUserEvaluationRules)
-//			Expect(err).ShouldNot(HaveOccurred())
-//
-//			rules, err := repository.GetAllUserEvaluationRulesToExecute()
-//			Expect(err).ShouldNot(HaveOccurred())
-//
-//			testsAfter := flatTests(rules)
-//
-//			flags := make([]bool, len(testsAfter))
-//
-//			for i, testBefore := range testsBefore {
-//				for _, testsAfter := range testsAfter {
-//					if testBefore.Id == testsAfter.Id {
-//						Expect(testBefore.CronId).To(Equal(testsAfter.CronId))
-//						flags[i] = true
-//						break
-//					}
-//				}
-//			}
-//
-//			for i, flag := range flags {
-//				Expect(flag).To(Equal(true), fmt.Sprintf("Not flagged: %v\n", testsBefore[i]))
-//			}
-//		})
-//
-//		AfterEach(func() {
-//			// and we delete them to be sure we leave room for
-//			// other operations
-//			err := repository.DeleteUserEvaluationRules(getIdsFromUserEvaluationRuleList(availableUserEvaluationRules))
-//			Expect(err).ShouldNot(HaveOccurred())
-//		})
-//	})
-//
-//})
